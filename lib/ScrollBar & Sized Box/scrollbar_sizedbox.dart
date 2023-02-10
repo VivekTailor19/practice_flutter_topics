21eb547fedd6704018a5be0523dfe383d09e23c2
Vivek <tailorvivekgec19@gmail.com>
2023-02-10 00:30:49 -0800

    Initial commit

---
+++ lib/ScrollBar & Sized Box/scrollbar_sizedbox.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ScrollBar_SizedBox extends StatefulWidget {
+  const ScrollBar_SizedBox({Key? key}) : super(key: key);
+
+  @override
+  State<ScrollBar_SizedBox> createState() => _ScrollBar_SizedBoxState();
+}
+
+class _ScrollBar_SizedBoxState extends State<ScrollBar_SizedBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("Scroll & SizedBar",style: TextStyle(fontSize: 25,color: Colors.redAccent),),
+        centerTitle: true,
+      ),
+
+      body:  SingleChildScrollView(
+          child: Column(
+            children: [
+              Text("Hello",style: TextStyle(fontSize: 50,color: Colors.redAccent),),
+              SizedBox(height: 10,),
+              Container(height: 40, width: 60, color: Colors.pink,),SizedBox(height: 10,),
+              Container(height: 40, width: 60, color: Colors.green,),SizedBox(height: 10,),
+              Container(height: 40, width: 60, color: Colors.pink,),SizedBox(height: 10,),
+              Container(height: 400, width: 60, color: Colors.yellow,),SizedBox(height: 10,),
+              Text("Hello",style: TextStyle(fontSize: 50,color: Colors.redAccent),),SizedBox(height: 10,),
+              Container(height: 40, width: 60, color: Colors.pink,),SizedBox(height: 10,),
+              Container(height: 500, width: 60, color: Colors.yellow,),SizedBox(height: 10,),
+              Text("Hello",style: TextStyle(fontSize: 50,color: Colors.redAccent),),
+
+            ],
+
+          ),
+
+      ),
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:practice_flutter_topics/ScrollBar%20&%20Sized%20Box/scrollbar_sizedbox.dart';
+
+
+void main()
+{
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ScrollBar_SizedBox(),
+
+  )
+  );
+}
